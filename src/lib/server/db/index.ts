3ce099ef0f69e902ece604b0db99de81e4a8ce6b
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-12-08 15:31:12 +0100

    use static db/portal.db database path

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,4 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
-import { env } from '$env/dynamic/private';
-if (!env.DB_PATH) throw new Error('DB_PATH is not set');
-const client = new Database(env.DB_PATH);
+const client = new Database('db/portal.db');
 export const db = drizzle(client);
