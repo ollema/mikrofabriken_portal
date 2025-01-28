9efe57088b052ea089c3994ce1a86e769c1d30c0
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-01-28 11:48:39 +0100

    add new member equals helpers

--- src/lib/server/members.ts
+++ src/lib/server/members.ts
@@ -29,3 +29,53 @@ export function getMember(slackID: string): Member {
 	const members = getMembers();
 	return findMember(members, slackID);
 }
+
+export function areMembersEqual(a: Member, b: Member): boolean {
+	if (
+		a.crNumber !== b.crNumber ||
+		a.slackID !== b.slackID ||
+		a.name !== b.name ||
+		a.postalAdress !== b.postalAdress ||
+		a.postalCode !== b.postalCode ||
+		a.postalCity !== b.postalCity ||
+		a.email !== b.email ||
+		a.slackEmail !== b.slackEmail ||
+		a.phone !== b.phone
+	) {
+		return false;
+	}
+
+	if (!areArraysEqual(a.iceContacts, b.iceContacts)) return false;
+	if (!areArraysEqual(a.agreements, b.agreements)) return false;
+	if (!areArraysEqual(a.artifacts, b.artifacts)) return false;
+	if (!areArraysEqual(a.commissions, b.commissions)) return false;
+	if (!areArraysEqual(a.workPools, b.workPools)) return false;
+
+	if (a.company && b.company) {
+		return (
+			a.company.orgNum === b.company.orgNum &&
+			a.company.name === b.company.name &&
+			a.company.postalAdress === b.company.postalAdress &&
+			a.company.postalCode === b.company.postalCode &&
+			a.company.postalCity === b.company.postalCity &&
+			a.company.email === b.company.email &&
+			a.company.invoiceDefaultTo === b.company.invoiceDefaultTo &&
+			areArraysEqual(
+				a.company.invoiceExcludeCategoriesFromDefault,
+				b.company.invoiceExcludeCategoriesFromDefault
+			)
+		);
+	}
+
+	return a.company === b.company;
+}
+
+export function areAllMembersEqual(a: Members, b: Members): boolean {
+	if (a.length !== b.length) return false;
+	return a.every((member, index) => areMembersEqual(member, b[index]));
+}
+
+function areArraysEqual<T>(a: T[], b: T[]): boolean {
+	if (a.length !== b.length) return false;
+	return JSON.stringify(a) === JSON.stringify(b);
+}
