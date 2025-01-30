db314506677595f2ea0a5381f2f90449a8866fcb
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-01-30 10:02:09 +0100

    sort workpools by name

--- src/routes/admin/workpools/+page.server.ts
+++ src/routes/admin/workpools/+page.server.ts
@@ -1,12 +1,13 @@
 import { getUser } from '$lib/server/auth.js';
 import { getMember, getMembers } from '$lib/server/members.js';
-import { getWorkPools } from '$lib/server/workpools.js';
+import { getWorkPoolNames, getWorkPools } from '$lib/server/workpools.js';
 import { isCommissionActive, isMemberActive } from '$lib/utils/member.js';
 
 export const load = async ({ locals }) => {
 	const user = getUser(locals);
 	const member = getMember(user.slackID);
 	const workPools = getWorkPools();
+	const workPoolNameMapping = getWorkPoolNames();
 	const allMembers = getMembers();
 
 	const visibleWorkPools =
@@ -19,12 +20,14 @@ export const load = async ({ locals }) => {
 				);
 
 	return {
-		workPools: visibleWorkPools.map((pool) => ({
-			...pool,
-			members: allMembers
-				.filter((m) => m.workPools.includes(pool.id) && isMemberActive(m))
-				.sort((a, b) => a.name.localeCompare(b.name))
-				.map(({ name, email, phone }) => ({ name, email, phone }))
-		}))
+		workPools: visibleWorkPools
+			.sort((a, b) => workPoolNameMapping[a.id].localeCompare(workPoolNameMapping[b.id]))
+			.map((pool) => ({
+				...pool,
+				members: allMembers
+					.filter((m) => m.workPools.includes(pool.id) && isMemberActive(m))
+					.sort((a, b) => a.name.localeCompare(b.name))
+					.map(({ name, email, phone }) => ({ name, email, phone }))
+			}))
 	};
 };
