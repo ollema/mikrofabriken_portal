e60510535395ee3680a160fa0de73954739707ac
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-01-30 10:02:00 +0100

    require at least one work pool

--- src/routes/membership/profile/work-pools/edit/schema.ts
+++ src/routes/membership/profile/work-pools/edit/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const workPoolsFormSchema = z.object({
-	workPools: z.array(z.string())
+	workPools: z.array(z.string()).min(1, 'At least one work pool must be selected')
 });
 
 export type WorkPoolsFormSchema = typeof workPoolsFormSchema;
