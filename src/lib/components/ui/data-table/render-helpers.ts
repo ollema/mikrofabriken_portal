fa78e874b50a8d1c596f9c98069fc5a33f2e68f8
Oskar Adin <oskar.adin@polestar.com>
2025-05-26 12:08:58 +0200

    fix: update type parameter name in renderComponent function to lint

--- src/lib/components/ui/data-table/render-helpers.ts
+++ src/lib/components/ui/data-table/render-helpers.ts
@@ -77,8 +77,8 @@ export class RenderSnippetConfig<TProps> {
 export function renderComponent<
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	T extends Component<any>,
-	Props extends ComponentProps<T>
->(component: T, props: Props = {} as Props) {
+	TProps extends ComponentProps<T>
+>(component: T, props: TProps = {} as TProps) {
 	return new RenderComponentConfig(component, props);
 }
 
