4cb91f5930b9e04d6c375e999bb43ddac871e554
Bjorn Bringert <bjorn@bringert.net>
2025-09-11 17:25:25 +0200

    Handle missing COG member presence gracefully (#38)

--- src/lib/utils/cog.ts
+++ src/lib/utils/cog.ts
@@ -1,4 +1,5 @@
 import type { Claims } from '$lib/types/cog.js';
+import { getOpenPeriods } from '$lib/server/cog';
 
 export function hasClaim(claims: Claims | undefined, resource: string, action: string): boolean {
 	if (claims === undefined) {
@@ -12,4 +13,22 @@ export function hasClaim(claims: Claims | undefined, resource: string, action: s
 	}
 
 	return false;
+}
+
+// gets all the members who are currently present (i.e. have an open period in the room resource)
+export async function getPresentMembers(): Promise<Record<string, boolean>> {
+	try {
+		const periods = await getOpenPeriods('room');
+		const here = periods.reduce(
+			(acc: Record<string, boolean>, period) => {
+				acc[period.memberCrNumber] = true;
+				return acc;
+			},
+			{} as Record<string, boolean>
+		);
+		return here;
+	} catch (e) {
+		console.error('could not fetch member presence status', e);
+		return {};
+	}
 }
--- src/routes/mikrofabriken/members/+page.server.ts
+++ src/routes/mikrofabriken/members/+page.server.ts
@@ -1,22 +1,13 @@
 import { getFormattedMembers } from '../utils.js';
 import { getUser } from '$lib/server/auth.js';
-import { getOpenPeriods } from '$lib/server/cog.js';
 import { getMembers } from '$lib/server/members.js';
+import { getPresentMembers } from '$lib/utils/cog.js';
 
 export const load = async ({ locals, url }) => {
 	getUser(locals, url);
 
 	const members = getMembers();
-
-	const periods = await getOpenPeriods('room');
-
-	const here = periods.reduce(
-		(acc: Record<string, boolean>, period) => {
-			acc[period.memberCrNumber] = true;
-			return acc;
-		},
-		{} as Record<string, boolean>
-	);
+	const here = await getPresentMembers();
 
 	return {
 		members: await getFormattedMembers(members, here)
