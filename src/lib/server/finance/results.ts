45c84266bac6130261d35799ce63b18cc07a3e1e
Bjorn Bringert <bjorn@bringert.net>
2025-11-05 23:33:22 +0100

    Add functions for computing results by cost center

---
+++ src/lib/server/finance/results.ts
@@ -0,0 +1,52 @@
+import type { Voucher } from "$lib/types/fortnox";
+
+type AccountTotals = {
+	debit: number;
+	credit: number;
+};
+
+type TotalsByAccount = Record<number, AccountTotals>;
+
+type TotalsByCostCenter = Record<string, TotalsByAccount>;
+
+type CostAndRevenue = {
+	cost: number;
+	revenue: number;
+};
+
+export async function getResultsForCurrentYear(vouchers: Voucher[]): Promise<TotalsByCostCenter> {
+    const results: TotalsByCostCenter = {};
+    for (const voucher of vouchers) {
+        for (const voucherRow of voucher.VoucherRows) {
+            const costCenter = voucherRow.CostCenter;
+            if (!results[costCenter]) {
+                results[costCenter] = {};
+            }
+            const account = voucherRow.Account;
+            if (!results[costCenter][account]) {
+                results[costCenter][account] = { debit: 0, credit: 0 };
+            }
+            results[costCenter][account].debit += voucherRow.Debit;
+            results[costCenter][account].credit += voucherRow.Credit;
+        }
+    }
+	return results;
+}
+
+// Sum cost and revenue accounts
+export async function sumResults(results: TotalsByCostCenter): Promise<Record<string, CostAndRevenue>> {
+    const summedResults: Record<string, CostAndRevenue> = {};
+    for (const costCenter in results) {
+        for (const account in results[costCenter]) {
+            if (!summedResults[costCenter]) {
+                summedResults[costCenter] = { cost: 0, revenue: 0 };
+            }
+            if (/^[3]/.test(account)) {
+                summedResults[costCenter].revenue += results[costCenter][account].debit - results[costCenter][account].credit;
+            } else if (/^[4567]/.test(account)) {
+                summedResults[costCenter].cost += results[costCenter][account].debit - results[costCenter][account].credit;
+            }
+        }
+    }
+    return summedResults;
+}
